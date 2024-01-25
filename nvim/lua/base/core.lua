52e9002b9391db6611315906f00da2e93b755df4
Andrius Miasnikovas <andrejus@vinted.com>
2024-01-25 12:09:33 +0200

    Autoresize neovim vertical split on terminal size change

--- nvim/lua/base/core.lua
+++ nvim/lua/base/core.lua
@@ -4,3 +4,11 @@ vim.g.maplocalleader = ','
 vim.cmd([[ autocmd BufRead,BufNewFile Jenkinsfile set filetype=groovy ]])
 
 vim.cmd([[ autocmd BufRead,BufNewFile Dangerfile set filetype=ruby ]])
+
+vim.api.nvim_create_autocmd("VimResized", {
+    pattern = '*',
+    callback = function()
+        vim.cmd('wincmd =')
+    end
+})
+
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,4 +2,13 @@ local wezterm = require("wezterm")
 
 local config = {}
 
+-- config.color_scheme = "Tokyo Night"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+-- config.font_size = 16.0 -- best fit for split screen
+config.font_size = 26.0
+
 return config
+
+-- 120 chars of ruler
+-- 45678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
