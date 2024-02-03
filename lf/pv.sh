737a049ff864383bad5ced4421783982f02d44fd
Andrius Miasnikovas <andriusms@gmail.com>
2024-02-04 01:45:43 +0200

    Update lf previewer

--- lf/pv.sh
+++ lf/pv.sh
@@ -4,11 +4,11 @@ case "$1" in
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
     *.rar) unrar l "$1";;
-    *.mp4) f="`basename $1`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" >/dev/null 2>&1 && viu -b "$o" ;;
-    *.mkv) f="`basename $1`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" >/dev/null 2>&1 && viu -b "$o" ;;
-    *.mpg) f="`basename $1`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" >/dev/null 2>&1 && viu -b "$o" ;;
-    *.m4v) f="`basename $1`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" >/dev/null 2>&1 && viu -b "$o" ;;
-    *.avi) f="`basename $1`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" >/dev/null 2>&1 && viu -b "$o" ;;
+    *.mp4) f="`basename \"$1\"`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" -s0 >/dev/null 2>&1 && viu -w 80 -b "$o" ;;
+    *.mkv) f="`basename \"$1\"`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" -s0 >/dev/null 2>&1 && viu -w 80 -b "$o" ;;
+    *.mpg) f="`basename \"$1\"`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" -s0 >/dev/null 2>&1 && viu -w 80 -b "$o" ;;
+    *.m4v) f="`basename \"$1\"`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" -s0 >/dev/null 2>&1 && viu -w 80 -b "$o" ;;
+    *.avi) f="`basename \"$1\"`" && o="/tmp/${f}_thumb.jpg" && ffmpegthumbnailer -i "$1" -o "$o" -s0 >/dev/null 2>&1 && viu -w 80 -b "$o" ;;
     *.jpg) viu -b -h "$3" "$1" ;;
     *.jpeg) viu -b -h "$3" "$1" ;;
     *.png) viu -b -h "$3" "$1" ;;
