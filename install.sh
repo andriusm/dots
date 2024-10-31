911b416e3556088e69aeb59856ed928f9c3cc50a
Andrius Miasnikovas <andriusms@gmail.com>
2024-10-31 22:08:13 +0200

    Add/update configs

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURDIR="$(pwd)"
+
+link() {
+	target="$HOME/.config/$1"
+	if [ -d $target ]; then
+		echo "Did not create link for '$1' (already exists)"
+	else
+		ln -s $CURDIR/$1 $target
+	fi
+}
+
+link "lf"
+link "alacritty"
+link "tmux"
+link "wezterm"
+link "nvim"
+link "git"
+link "dosbox"
+link "bash"
+link "btop"
+link "i3"
