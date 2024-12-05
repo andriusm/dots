23a5d0b23d4f7fa0d69b1bafe766341209345a80
Andrius Miasnikovas <andriusms@gmail.com>
2024-12-05 22:07:04 +0200

    Handle ft specific config differently in neovim

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,1 +1,6 @@
 vim.opt.tabstop = 4
+
+vim.keymap.set("n", "<localleader>r", '<cmd>!go run ./...<CR>')
+vim.keymap.set("n", "<localleader>t", '<cmd>GoTest<CR>')
+vim.keymap.set("n", "<localleader>b", '<cmd>GoBuild<CR>')
+vim.keymap.set("n", "<localleader>j", '<cmd>lua AltGoFile()<CR>')
---
+++ nvim/after/ftplugin/ruby.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<localleader>x", '<cmd>lua DbMigrate()<CR>')
+vim.keymap.set("n", "<localleader>c", '<cmd>lua BundleInstall()<CR>')
+vim.keymap.set("n", "<localleader>j", '<cmd>lua AltRubyFile()<CR>')
+vim.keymap.set("n", "<localleader>m", '<cmd>lua PackwerkPackageFile()<CR>')
